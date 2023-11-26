3ed27cc917f007849b60342b2f23fde6cdd77d63
Denis0001 <4208744q@gmail.com>
2023-11-26 19:15:05 +0200

    Create project

---
+++ pages/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView,ListView
+from .models import Post
+ 
+class HomePageView(ListView):
+    template_name = 'home.html'
+    model = Post
+
+class AboutPageView(TemplateView): # новое
+    template_name = 'about.html'
+
